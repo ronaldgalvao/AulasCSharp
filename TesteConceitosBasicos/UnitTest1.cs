4ebc97d23eeff11da7ba133a7f9803979ceb90da
rosenclever <rosenclever@gmail.com>
2024-03-13 20:14:30 -0300

    teste obter classificacao ok

---
+++ TesteConceitosBasicos/UnitTest1.cs
@@ -0,0 +1,36 @@
+using ConceitosBasicos;
+
+namespace TesteConceitosBasicos
+{
+    [TestClass]
+    public class CalculadoraIMCTeste
+    {
+        [TestMethod]
+        public void CalcularTeste()
+        {
+            //cenario
+            float peso = 100f;
+            float altura = 2f;
+            CalculadoraIMC calculadora = new CalculadoraIMC();
+
+            //ação
+            float imc  = calculadora.Calcular(peso, altura);
+            //separar os métodos calcular imc e obter status na classe CalcularIMC
+            //assistir ao vídeo sobre príncípios solid do Felipe Deschamps
+            //verificação
+            Assert.AreEqual(25, imc);
+        }
+        [TestMethod]
+        public void ObterClassificacaoTest() 
+        {
+            //cenario
+            float imc = 25;
+            string status;
+            CalculadoraIMC calculadoraIMC = new CalculadoraIMC();
+            //ação
+            status = calculadoraIMC.ObterClassificacao(imc);
+            //verificação
+            Assert.AreEqual("sobrepeso", status);
+        }   
+    }
+}
